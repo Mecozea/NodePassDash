a96d13b5d000128cb4a57d6193b2958c768c9ff2
yangtao <yangtao@drooloo.cn>
2025-09-13 00:15:29 +0800

    fix: 修复

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { readFileSync } from "fs";
+import tailwindcss from "@tailwindcss/vite"
 
 // 读取package.json获取版本号
 const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
     __DEV_MODE__: JSON.stringify(mode === 'development'),
